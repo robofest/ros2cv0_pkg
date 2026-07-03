ded93d8ca33d46d487d5ded8abd12ec0b02c0e49
CJ Chung <cchung@ltu.edu>
2026-07-02 22:14:39 -0400

    launch file debugged.

--- launch/bag2image_view.launch.py
+++ launch/bag2image_view.launch.py
@@ -1,20 +1,24 @@
+from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import ExecuteProcess
+from launch.actions import ExecuteProcess, SetEnvironmentVariable
 
 def generate_launch_description():
     
+    # Mute the noisy Qt warning messages
+    quiet_qt = SetEnvironmentVariable('QT_LOGGING_RULES', '*.warning=false')
+    
     # 1. Path to your bag file directory (Replace with your actual path)
     # Example: '/home/user/bag_files/my_camera_data'
-    bag_file_path = 'PATH_TO_YOUR_BAG_FILE_DIRECTORY'
+    bag_file_path = '/home/developer/rosbags/L_7pm_bag'
 
     # 2. Process to play the rosbag file
-    ExecuteProcess(
+    rosbag_play = ExecuteProcess(
         cmd=['ros2', 'bag', 'play', bag_file_path, '--loop'],  
         output='screen'
     )
 
     # 3. Your image viewer node listening to the bag file's topic
-    Node(
+    image_view_node = Node(
         package='ros2cv0_pkg',
         executable='image_view_exe',
         name='image_view',
@@ -24,3 +28,9 @@ def generate_launch_description():
             {'cam_topic': '/cam_pub/image_raw'}
         ]
     )
+
+    return LaunchDescription([
+        quiet_qt,
+        rosbag_play,
+        image_view_node
+    ])
